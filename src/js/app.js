c080785e73536f422550c43f5e740cc7f8ce6c73
Alexander Sabanov <alexsab@mail.ru>
2025-03-31 22:17:07 +0700

    upd lead link

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ import './modules/stock-slider';
 import ResponsiveMenu from './modules/ResponsiveMenu';
 import { connectForms, cookiecook } from '@alexsab-ru/scripts';
 cookiecook();
-connectForms('https://alexsab.ru/lead/dev/', {
+connectForms('https://alexsab.ru/lead/gac/samara/', {
 	confirmModalText: 'Вы уже оставляли заявку сегодня, с Вами обязательно свяжутся в ближайшее время!',
 });
 
@@ -57,7 +57,7 @@ if(lazys.length){
 
 function executeRecaptcha() {
 grecaptcha.ready(function() {
-	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'gac_alexsab'}).then(function(token) {
+	grecaptcha.execute('6Lepfy4pAAAAAAGHFP655qNe6Bb_BcskklcxajC6', {action: 'gac_samara'}).then(function(token) {
 		let formData = new FormData();
 		formData.append('g-recaptcha-response', token);
 		const params = new URLSearchParams([...formData]);
